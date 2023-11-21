e62d4acee7d4c24b415c9d02b67504cae079f8d3
Keith <shidingye@dibgroup.cn>
2023-11-21 23:15:05 +0800

    【创建虚拟】

--- packages/runtime-core/src/vnode.ts
+++ packages/runtime-core/src/vnode.ts
@@ -1,4 +1,19 @@
+//创建vnode 1.createVnode和h()作用一样 需要区分是组件还是元素
 
-export const createVnode = (rootComponent,rootProps)=> {
-    console.log(rootComponent,rootProps)
+export const createVnode = (type,props,children=null)=> {
+    // console.log(rootComponent,rootProps)
+
+    //区分 是组件还是元素
+    let shapeFlag
+    const vnode = {
+        _v_isVnode:true,//是一个vnode节点
+        type,
+        props,
+        children,
+        key:props && props.key,//diff
+        el:null,//dom和vnode对应
+        shapeFlag
+    }
+    
+    return vnode;
 }
